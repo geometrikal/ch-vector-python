7ef13d1ecbe955c007ef98d323b3af56b2ae099a
Ross Marchant <ross.g.marchant@gmail.com>
2020-06-10 22:28:27 +1000

    Fixed log-gabor not having 0 DC

--- chvector/filters/filters.py
+++ chvector/filters/filters.py
@@ -7,6 +7,7 @@ def log_gabor_spectrum(shape, wavelength, sigma, passband='band'):
     _,_, r, _ =  fft.fft_mesh(shape)
     ops.dc_to_value(r, 1)
     s = np.exp((-(np.log(r * wavelength)) ** 2) / (2 * np.log(sigma) ** 2))
+    ops.dc_to_zero(s)
     if passband == 'high':
         s[r >= 1/wavelength] = 1
     elif passband == 'low':
