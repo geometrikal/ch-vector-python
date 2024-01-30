9b933a73a6a3a643b7082dbe6cb4d1ad3a9e732e
geometrikal <ross.g.marchant@gmail.com>
2024-01-30 12:04:24 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chvector',
-    version='',
+    version='1.0.0',
     packages=['chvector', 'chvector.test', 'chvector.utils', 'chvector.models', 'chvector.filters', 'chvector.transforms'],
     url='',
     license='',
